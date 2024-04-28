44b771ff323479db8323ee2e02e54eaf39bb5349
Fabrizio Giammatteo <fabrizio.giammatteo@gmail.com>
2024-04-28 20:30:40 +0200

    Fix

--- gocommons.go
+++ gocommons.go
@@ -2,6 +2,7 @@ package gocommons
 
 import (
 	"bufio"
+	"bytes"
 	"context"
 	"crypto/aes"
 	"crypto/cipher"
@@ -178,6 +179,40 @@ func (qe *CustomError) Error() string {
 
 func (qe *CustomError) GetCode() int {
 	return qe.StatusCode
+}
+
+type TelegramMessage struct {
+	ChatID int64  `json:"chat_id"`
+	Text   string `json:"text"`
+}
+
+type Telegram struct {
+	Url string `json:"url"`
+}
+
+func (t *Telegram) SetUrl(token string) {
+	t.Url = fmt.Sprintf("https://api.telegram.org/bot%s/sendMessage", token)
+}
+
+// SendMessage sends a message to given URL.
+func (t *Telegram) SendMessage(message *TelegramMessage) error {
+	payload, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+	response, err := http.Post(t.Url, "application/json", bytes.NewBuffer(payload))
+	if err != nil {
+		return err
+	}
+	defer func(body io.ReadCloser) {
+		if err := body.Close(); err != nil {
+			log.Println("failed to close response body")
+		}
+	}(response.Body)
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to send successful request. Status was %q", response.Status)
+	}
+	return nil
 }
 
 var DateLayout string = "2006-01-02 15:04:05"
@@ -1457,4 +1492,10 @@ func GetCryptoStuff(cryptoItem string) (string, []byte, error) {
 	}
 
 	return cryptoKey, nonce, nil
+}
+
+func NewTelegram(token string) Telegram {
+	t := Telegram{}
+	t.SetUrl(token)
+	return t
 }
